251e7295fc708a7a7561aed8b16fe1553cfcba16
ivdenvas <ivdenvas@gmail.com>
2024-02-01 17:58:25 +0300

    f

---
+++ src/main/java/com/example/CRUDapp/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.example.CRUDapp.controller;
+
+
+import com.example.CRUDapp.model.User;
+import com.example.CRUDapp.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model){
+        List<User> users = userService.findAll();
+
+        model.addAttribute("users", users);
+        return "user-list";
+        //return "home.html";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser (@PathVariable("id") int id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String updateUserForm(@PathVariable("id") int id, Model model){
+        model.addAttribute("user",userService.getOne(id));
+        return "user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(@ModelAttribute("user") User user){
+        userService.update(user);
+        return "redirect:/users";
+    }
+}
